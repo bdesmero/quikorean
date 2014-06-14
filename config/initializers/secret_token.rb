ee5e03c9ad4cb0f2d457be0f8512c14be67fc5ed
bdesmero <bdesmero@gmail.com>
2014-06-14 16:26:39 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quikorean::Application.config.secret_key_base = '58a06077feb62bfe7abe6fef72b01681eb710e4f920ecd739397e591d4cee9c507c75145454757110090522c7596086c7f8f0d25728b009db4b4e232788642cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quikorean::Application.config.session_store :cookie_store, key: '_quikorean_session'
